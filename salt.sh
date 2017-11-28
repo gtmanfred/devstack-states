57a29f1397a489caf6029c519d163951417a331c
Daniel Wallace <danielwallace@gtmanfred.com>
2017-11-27 17:23:54 -0700

    set src path for pip editable install

--- salt.sh
+++ salt.sh
@@ -3,12 +3,13 @@
 #    --image-family centos-7 \
 #    --image-project centos-cloud \
 #    --metadata-from-file startup-script=salt.sh \
+while ! getent passwd daniel; do sleep 1; done
 groupadd salt
 gpasswd -a daniel salt
 directories=(/etc/salt /var/cache/salt /var/log/salt /var/run/salt /etc/openstack /etc/salt/cloud.{providers,profiles,deploy}.d)
 mkdir -p "${directories[@]}"
 yum install -y python-pip python-devel gcc git systemd-python
-sudo -u daniel pip install --user -e git+git://github.com/gtmanfred/salt.git@shade#egg=salt shade
+sudo -u daniel pip install --user --src /home/daniel/src -e git+git://github.com/gtmanfred/salt.git@shade#egg=salt shade
 for dir in minion master; do
     cp -a "/home/daniel/src/salt/pkg/salt-${dir}.service" /etc/systemd/system/
     mkdir "/etc/systemd/system/salt-${dir}.service.d"
